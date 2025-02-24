57f8209123796a6d6d26632671a4b47c313855a0
Arturo Esqueda <art270112@gmail.com>
2025-02-24 16:24:01 -0600

    Se termina con la practica del dashboard

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,6 +1,6 @@
 
 import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
-import { notFound } from "next/navigation";
+
 
 const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
     const data: PokemonsResponse = await fetch (`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).then(res => res.json());
